1493283b410839ef50b35c2682254df5146b4079
Vincent MONTÉCOT <vincent.montecot@sharingcloud.com>
2024-01-05 14:09:23 +0100

    cleared a bit

---
+++ tests/bases/naming_conventions_more_complete/subdir1/not_collected_tests.py
@@ -0,0 +1,33 @@
+def basic_test():
+    pass
+
+
+def test_basic():
+    pass
+
+
+class TestMyTestclass:
+    def basic_test(self):
+        pass
+
+    def test_basic(self):
+        pass
+
+
+class MyTestclass:
+    def basic_test(self):
+        pass
+
+    def test_basic(self):
+        pass
+
+
+class TestMyTestclassWithInit:
+    def __init__(self):
+        pass
+
+    def basic_test(self):
+        pass
+
+    def test_basic(self):
+        pass
---
+++ tests/trucs/cache/subdir1/test_foo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def test_success():
+    pass
+
+def test_fail():
+    assert False
